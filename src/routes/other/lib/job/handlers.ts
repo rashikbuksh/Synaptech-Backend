4c2c12a7238c023939e18b7d2947862d9e7c952d
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-07-07 19:07:39 +0600

    fix: update valueLabel handler to switch between job and loan schemas

--- src/routes/other/lib/job/handlers.ts
+++ src/routes/other/lib/job/handlers.ts
@@ -3,16 +3,16 @@ import type { AppRouteHandler } from '@/lib/types';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
-import { loan } from '@/routes/lib/schema';
+import { job } from '@/routes/lib/schema';
 
 import type { ValueLabelRoute } from './routes';
 
 export const valueLabel: AppRouteHandler<ValueLabelRoute> = async (c: any) => {
   const resultPromise = db.select({
-    value: loan.uuid,
-    label: loan.lender_name,
+    value: job.uuid,
+    label: job.work_order,
   })
-    .from(loan);
+    .from(job);
 
   const data = await resultPromise;
 
