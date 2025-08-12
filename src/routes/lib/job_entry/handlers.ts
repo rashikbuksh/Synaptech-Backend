6f3a86bb4d766c13e680a11fa8a2867c372a8305
Rashik Buksh <rashikbuksh@gmail.com>
2025-08-12 15:37:48 +0600

    feat: add deletion of associated product_serial records in job_entry removal

--- src/routes/lib/job_entry/handlers.ts
+++ src/routes/lib/job_entry/handlers.ts
@@ -1,17 +1,16 @@
 import type { AppRouteHandler } from '@/lib/types';
 
-import { asc, eq, or } from 'drizzle-orm';
+import { asc, eq } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
-import nanoid from '@/lib/nanoid';
 import { PG_DECIMAL_TO_FLOAT } from '@/lib/variables';
 import { users } from '@/routes/hr/schema';
 import { createToast, DataNotFound, ObjectNotFound } from '@/utils/return';
 
 import type { CreateRoute, GetOneRoute, ListRoute, PatchRoute, RemoveRoute } from './routes';
 
-import { job, job_entry, product, vendor } from '../schema';
+import { job, job_entry, product, product_serial, vendor } from '../schema';
 
 export const create: AppRouteHandler<CreateRoute> = async (c: any) => {
   const value = c.req.valid('json');
@@ -129,6 +128,12 @@ export const patch: AppRouteHandler<PatchRoute> = async (c: any) => {
 
 export const remove: AppRouteHandler<RemoveRoute> = async (c: any) => {
   const { uuid } = c.req.valid('param');
+
+  const _data2 = await db.delete(product_serial)
+    .where(eq(product_serial.job_entry_uuid, uuid))
+    .returning({
+      name: product_serial.uuid,
+    });
 
   const [data] = await db.delete(job_entry)
     .where(eq(job_entry.uuid, uuid))
